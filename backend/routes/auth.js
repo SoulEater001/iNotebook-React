68bd77a3093669d69b55863f363a2fbbe1d97c48
SoulEater001 <dshivam698@gmail.com>
2024-12-09 04:23:44 +0530

    Added express-validator

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,10 +1,31 @@
 const express = require('express');
-const User = require('../models/User.js')
+const { body, validationResult } = require('express-validator');
 const router = express.Router();
+const User = require("../models/User.js")
 
-router.get('/', (req, res) => {
-    const user = User(req.body);
-    user.save();
-    res.send(req.body);
-})
+router.post(
+    '/',
+    [
+        body('name', "Enter a valid name").isLength({ min: 3 }),
+        body('email', "Enter a valid email").isEmail(),
+        body('password', "Enter a valid password").isLength({ min: 5 }),
+    ],
+    (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+        User.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password,
+        }).then(user => {
+            res.status(200).json({ message: 'User created successfully!', user });
+        }).catch(err => {
+            console.log(err);
+            res.status(500).json({ message: 'Please enter a unique value', error: err.message });
+        });
+
+    });
+
 module.exports = router;
