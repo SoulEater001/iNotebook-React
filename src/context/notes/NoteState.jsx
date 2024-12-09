6dae1382a1c3763bccd29a3fa04acf62d7cd38ba
SoulEater001 <dshivam698@gmail.com>
2024-12-10 02:14:19 +0530

    Added addnote component, small error fixes and refactoring

--- src/context/notes/NoteState.jsx
+++ src/context/notes/NoteState.jsx
@@ -61,8 +61,33 @@ const NoteState = (props) => {
         ]
 
     const [notes, setNotes] = useState(notesInitial);
+
+    //Add a note
+    const addNote = (title, description, tag) => {
+        console.log("Adding a note")
+        const note = {
+            "_id": "6756eb4151603efa9d547a3f",
+            "user": "6756dd6d9dca2ac149b1f881",
+            "title": title,
+            "description": description,
+            "tag": tag,
+            "date": "2024-12-09T13:06:09.855Z",
+            "__v": 0
+        };
+        setNotes(notes.concat(note));
+    }
+    //Delete a note
+    const deleteNote = () => {
+
+    }
+
+    //Edit a note
+    const editNote = () => {
+
+    }
+
     return (
-        <NoteContext.Provider value={{ notes, setNotes }}>
+        <NoteContext.Provider value={{ notes, setNotes, addNote, deleteNote, editNote }}>
             {props.children}
         </NoteContext.Provider>
     )
