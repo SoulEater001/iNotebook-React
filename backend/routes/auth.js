4c15787649f7e451354d91406cb414df6496d188
SoulEater001 <dshivam698@gmail.com>
2024-12-09 17:38:34 +0530

    Added password hashing and token authentication

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,7 +2,10 @@ const express = require('express');
 const { body, validationResult } = require('express-validator');
 const router = express.Router();
 const User = require("../models/User.js")
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
+const JWT_Secret = "ashdkriwl24398u7nfdgk";
 router.post(
     '/createUser',
     [
@@ -22,12 +25,21 @@ router.post(
             if (user) {
                 return res.status(400).json({ error: "Sorry a user with this email already exists" })
             }
+            const salt = await bcrypt.genSaltSync(10);
+            const secPass = await bcrypt.hash(req.body.password, salt);
+
             user = await User.create({
                 name: req.body.name,
                 email: req.body.email,
-                password: req.body.password,
+                password: secPass,
             })
-            res.json(user)
+            const data = {
+                user: {
+                    id: user.id
+                }
+            }
+            const authToken = jwt.sign(data, JWT_Secret);
+            res.json({ authToken });
         } catch (error) {
             console.log(error.message);
             res.status(500).send("Some error occured");
