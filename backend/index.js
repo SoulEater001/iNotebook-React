3b6047c2971ea48f8ba4b07bb6a1e66d17952ef5
SoulEater001 <dshivam698@gmail.com>
2024-12-10 04:11:31 +0530

    Added cors and fetchnote functionality

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,11 @@
 const connectToMongo = require("./db.js");
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
 const port = 5000;
 app.use(express.json());
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.get('/', (req, res) => {
     res.send('Hello World!')
