4ea636a1f6439dedbd4c14cbc62166d2050cb228
SoulEater001 <dshivam698@gmail.com>
2024-12-10 00:09:20 +0530

    Navbar and routes setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import { useState } from 'react'
 import './App.css'
+import { Routes, Route } from 'react-router-dom'
+import Navbar from './component/Navbar'
+import Home from './component/Home'
+import About from './component/About'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>this is iNotebook</h1>
+      <Navbar />
+      <Routes>
+        <Route exact path='/' element={<Home />}></Route>
+        <Route exact path='/about' element={<About />}></Route>
+      </Routes>
     </>
   )
 }
